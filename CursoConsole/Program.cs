99d8edb48e9f7c5edd7357e676e145fbe4ff8b2c
kaoossefvc <kaoossefvc@hotmail.com>
2016-07-11 21:57:20 -0300

    segundo comit

--- CursoConsole/Program.cs
+++ CursoConsole/Program.cs
@@ -10,7 +10,34 @@ namespace CursoConsole
     {
         static void Main(string[] args)
         {
+            int opcao, operacao;
+            String cliente = "", produto = "";
+            Console.Clear();
+            Console.WriteLine("1- Cliente");
+            Console.WriteLine("1- Produto");
+            Console.WriteLine("1- Sair");
+            opcao = Convert.ToInt32(Console.ReadLine());
+            switch (opcao)
+            {
 
+                case 1:
+                    Console.WriteLine("1- Cadstrar cliente");
+                    Console.WriteLine("2- Consultar Cliente");
+                    break;
+                case 2:
+                    Console.WriteLine("1- Cadstrar produto");
+                    Console.WriteLine("2- Consultar produto");
+                    break;
+                case 3:
+                    Console.WriteLine("Saindo da Aplicação");
+                    break;
+                default:
+                    Console.WriteLine("operação invalida");
+                    break;
+
+
+            }
         }
+
     }
 }
